cf78dcba42ba43662509565c868aff427e04e19d
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-02-04 16:43:04 -0600

    Windows autopatch development. [ISE-66] 1. Remove dependency on autopatch. This has become unique enough that sitting atop the linux implementation is not needed. 2. Transition to attributes for the windows_task resource properties.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
-# use the same attributes as the linux side... Configure defaults here, which may be different than Linux
-default['auto-patch']['disable'] = false
-default['auto-patch']['hour'] = 3
-default['auto-patch']['minute'] = 0
-default['auto-patch']['monthly'] = 'third tuesday'
-default['auto-patch']['reboot'] = true
-default['auto-patch']['splay'] = 1800
-default['auto-patch']['weekly'] = nil
+default['autopatch-nativex']['disabled'] = false
+default['autopatch-nativex']['task_username'] = 'SYSTEM'
+default['autopatch-nativex']['task_frequency'] = :monthly
+default['autopatch-nativex']['task_frequency_modifier'] = 'THIRD'
+default['autopatch-nativex']['task_months'] = 'JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV'
+default['autopatch-nativex']['task_days'] = 'THU'
+default['autopatch-nativex']['task_start_time'] = '03:00'
+default['autopatch-nativex']['working_dir'] = 'C:\chef_autopatch'
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,62 +9,31 @@
 # This is based on the 'auto-patch' cookbook. The main difference is in the use of a scheduling mechanism other than 'cron'.
 # See https://github.com/bflad/chef-auto-patch/blob/3b581cfc75a9fbe22a7b6af5d9e3519facae0f4e/recipes/default.rb
 
-#include_recipe 'cron'
-
-unless node['auto-patch']['disable']
-  if node['auto-patch']['weekly']
-    node.set['auto-patch']['day'] = '*'
-    node.set['auto-patch']['month'] = '*'
-    node.set['auto-patch']['weekday'] = AutoPatch.weekday(node['auto-patch']['weekly'])
-    Chef::Log.info("Auto patch scheduled weekly on #{node['auto-patch']['weekly']} at #{node['auto-patch']['hour']}:#{node['auto-patch']['minute']}")
-  elsif node['auto-patch']['monthly']
-    next_date = AutoPatch.next_monthly_date(
-      node['auto-patch']['monthly'],
-      node['auto-patch']['hour'],
-      node['auto-patch']['minute'])
-    node.set['auto-patch']['day'] = next_date.day
-    node.set['auto-patch']['month'] = next_date.month
-    node.set['auto-patch']['weekday'] = '*'
-    Chef::Log.info("Auto patch scheduled for #{next_date.strftime('%Y-%m-%d')} at #{node['auto-patch']['hour']}:#{node['auto-patch']['minute']}")
-  else
-    Chef::Application.fatal!('Missing auto-patch monthly or weekly specification.')
-  end
-end
-
-#template '/usr/local/sbin/auto-patch' do
-#  source 'auto-patch.sh.erb'
+directory "Auto Patch Working Directory" do
+  path node['autopatch-nativex']['working_dir']
 #  owner 'root'
 #  group 'root'
 #  mode '0700'
-#  action :delete if node['auto-patch']['disable']
-#end
+  action :create
+end
 
-template 'C:\auto-patch.ps1' do
+template 'AutoPatch PowerShell Script' do
   source 'auto-patch.ps1.erb'
+  path "#{node['autopatch-nativex']['working_dir']}\\auto-patch.ps1"
 #  owner 'root'
 #  group 'root'
 #  mode '0700'
-  action :delete if node['auto-patch']['disable']
+  action :delete if node['autopatch-nativex']['disable']
 end
-
-#cron_d 'auto-patch' do
-#  hour node['auto-patch']['hour']
-#  minute node['auto-patch']['minute']
-#  weekday node['auto-patch']['weekday']
-#  day node['auto-patch']['day']
-#  month node['auto-patch']['month']
-#  command '/usr/local/sbin/auto-patch'
-#  action :delete if node['auto-patch']['disable']
-#end
 
 windows_task 'autopatch' do
-  user 'SYSTEM'
-  frequency :monthly
-  frequency_modifier 'THIRD'
-  day 'THU'
-  months 'JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV'
-  start_time '03:00'
+  user node['autopatch-nativex']['task_username']
+  frequency node['autopatch-nativex']['task_frequency']
+  frequency_modifier node['autopatch-nativex']['task_frequency_modifier']
+  day node['autopatch-nativex']['task_days']
+  months node['autopatch-nativex']['task_months']
+  start_time node['autopatch-nativex']['task_start_time']
   cwd 'C:'
-  command 'PowerShell -Command "C:\auto-patch.ps1" -ExecutionPolicy Bypass'
-  action :delete if node['auto-patch']['disable']
+  command "PowerShell -Command \"#{node['autopatch-nativex']['working_dir']}\\auto-patch.ps1\" -ExecutionPolicy Bypass"
+  action :delete if node['autopatch-nativex']['disable']
 end
